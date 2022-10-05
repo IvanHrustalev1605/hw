3eb00f7b4f409137df1c0c70862d2ae9b7f96de0
Ivan_Khrustalev <khrustalev16@bk.ru>
2022-10-05 15:34:32 +0300

    my_project

--- MyDz5/controllers/aside.php
+++ MyDz5/controllers/aside.php
@@ -1,6 +1,6 @@
 <?php
 $left = template('main/v_aside');
-$content = temlate('main/v_asideCont');
+$content = template('main/v_asideCont');
 
 $pageContent = $template('main/v_main', [
     'content' => $content,
